ee5f3473c53c3af9e87a0af31501f05962f0dde3
Narcis13 <Narcis75@gmail.com>
2026-01-12 20:38:48 +0200

    re youtube

--- src/Middleware/SecurityHeadersMiddleware.php
+++ src/Middleware/SecurityHeadersMiddleware.php
@@ -65,12 +65,12 @@ class SecurityHeadersMiddleware implements MiddlewareInterface
         $directives = [
             "default-src 'self'",
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' " .
-                'https://cdn.jsdelivr.net https://cdn.tiny.cloud https://cdnjs.cloudflare.com https://cdn.plyr.io',
+                'https://cdn.jsdelivr.net https://cdn.tiny.cloud https://cdnjs.cloudflare.com https://cdn.plyr.io https://www.youtube.com',
             "style-src 'self' 'unsafe-inline' " .
                 'https://fonts.googleapis.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://cdn.tiny.cloud https://cdn.plyr.io',
             "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com data:",
             "img-src 'self' data: https:",
-            "connect-src 'self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://cdn.tiny.cloud",
+            "connect-src 'self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://cdn.tiny.cloud https://cdn.plyr.io",
             "frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com",
             "frame-ancestors 'self'",
             "base-uri 'self'",
